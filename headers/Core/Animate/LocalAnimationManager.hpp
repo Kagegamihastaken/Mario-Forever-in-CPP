184ac0a5f0ea953d3e3e72b5d55d6d5a3c7a18eb
Kagegami <doviethuy44@gmail.com>
2025-05-15 15:15:28 +0700

    fixed view seems glitching

---
+++ headers/Core/Animate/LocalAnimationManager.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef LOCALANIMATIONMANAGER_HPP
+#define LOCALANIMATIONMANAGER_HPP
+
+#include <SFML\Graphics.hpp>
+
+class LocalAnimationManager {
+private:
+	int indexAnimation = 0;
+	int startingIndexAnimation = 0;
+	int endingIndexAnimation = 0;
+	int frequency = 50;
+	int y = 0;
+	sf::Clock TimeRun;
+	float TimeRan = 0.0f;
+	float TimeRemainSave = 0.0f;
+	int sizex = 0, sizey = 0;
+public:
+	void setAnimation(int startingIndexAnimation = 0, int endingIndexAnimation = 0, int sizex = 0, int sizey = 0, int y = 0, int frequency = 50);
+	void SetRangeIndexAnimation(int startingIndexAnimation, int endingIndexAnimation, int frequency = 50);
+	void setIndexAnimation(int indexAnimation);
+	void setStartingIndexAnimation(int startingIndexAnimation);
+	void setEndingIndexAnimation(int endingIndexAnimation);
+	void setFrequency(int frequency);
+	void update(sf::Sprite& sprite);
+	void silentupdate();
+	bool isAtTheEnd() const;
+
+	static void setTexture(sf::Sprite& sprite, const sf::Texture* texture);
+	void setYPos(int y);
+};
+#endif // LOCALANIMATIONMANAGER_HPP
---
+++ sources/BgGradient.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+#include "Core/Background/BgGradient.hpp"
+#include "Core/WindowFrame.hpp"
+#include "Core/Level.hpp"
+
+auto bgGradient = sf::VertexArray(sf::PrimitiveType::TriangleStrip, 4);
+void BgGradientInitPos() {
+	//bgGradient[0].position = sf::Vector2f(ViewX - (view.getSize().x - Width), ViewY - (view.getSize().y - Height));
+	//bgGradient[1].position = sf::Vector2f(ViewX + view.getSize().x, ViewY - (view.getSize().y - Height));
+	//bgGradient[2].position = sf::Vector2f(ViewX - (view.getSize().x - Width), ViewY + view.getSize().y);
+	//bgGradient[3].position = sf::Vector2f(ViewX + view.getSize().x, ViewY + view.getSize().y);
+
+	bgGradient[0].position = sf::Vector2f(0, 0);
+	bgGradient[1].position = sf::Vector2f(LevelWidth, 0);
+	bgGradient[2].position = sf::Vector2f(0, LevelHeight);
+	bgGradient[3].position = sf::Vector2f(LevelWidth, LevelHeight);
+}
+void BgGradientDraw() {
+	rObject.draw(bgGradient);
+}
